738569ab922ceefb6ae6172ad6f7f7b50c20519a
Thomas Newcomb <volunshade@gmail.com>
2023-05-08 18:57:23 -0700

    Setup login/logout system, setup User Profile and update system.

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -121,9 +121,15 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+MEDIA_ROOT = BASE_DIR.joinpath('media')
+MEDIA_URL = 'media/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+LOGIN_REDIRECT_URL = 'blog-home'
+LOGIN_URL = 'login'
